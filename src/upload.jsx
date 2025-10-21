08e21886546cbf5bfff5ca27e719b015173e7fdf
Allsnowyo7 <edelvalle26@vcschools.org>
2025-10-20 17:11:34 -0700

    WIP

--- src/upload.jsx
+++ src/upload.jsx
@@ -1,20 +1,27 @@
 // https://docs.google.com/spreadsheets/d/16ECac40fAYy9EGW1IKXw_YhP9tcOaUG_pzuOk9Pg74M/edit?usp=sharing
 
 export function SendToSheets() {
+    const formData = new FormData();
+    formData.append("test", 1);
     function update() {
         fetch(
-            "https://sheets.googleapis.com/v4/spreadsheets/16ECac40fAYy9EGW1IKXw_YhP9tcOaUG_pzuOk9Pg74M/values/{range}",
+            "https://script.google.com/macros/s/AKfycbwNluKKBqlj7pUOjcYYnyCfO86rPxd5E4FJixmpldCHjkY5u8LP2woVBfJNnamv1gA/exec",
             {
-                method: 'PUT'
-                body: {
-                    `1972334219!A1:B2`
-                    JSON.stringify(["test"])
-                }
+                method: 'POST',
+                body: formData
             }
-        )
-    }   
-    return( <div>
-        <button onClick = {update()} />
-        </div>
-    )
+        ).then(response => response.text())
+            .then(result => {
+                console.log('Success:', result);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
+    return (<div>
+        <h1>
+        <button onClick={update()} />
+        </h1>
+    </div>
+    )
+}
