78207801990fa98770fba08c247e0d7fba754f26
Elisha <edelvalle26@vcschools.org>
2025-10-21 20:19:12 -0700

    added ghpages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { ScoreButton } from './ScoreButton';
 import {useState} from 'react';
-import {DownloadJSON} from './export';
+import axios from 'axios';
 
 
 
@@ -10,18 +10,24 @@ const [countL1, setCountL1] = useState(0);
 const [countL2, setCountL2] = useState(0);
 const [countL3, setCountL3] = useState(0);
 const [countL4, setCountL4] = useState(0);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const objt = ["Match: "];
+
+  axios
+    .post(
+      'https://script.google.com/macros/s/AKfycbwNluKKBqlj7pUOjcYYnyCfO86rPxd5E4FJixmpldCHjkY5u8LP2woVBfJNnamv1gA/exec',
+      ["Match: 1"]
+    )
+    .then((response) => {
+      console.log(response);
+    });
+};
  return (
    <div>
-      <ScoreButton count = {countL1} setCount={setCountL1} />
-      <ScoreButton count = {countL2} setCount={setCountL2} />
-      <ScoreButton count = {countL3} setCount={setCountL3} />
-      <ScoreButton count = {countL4} setCount={setCountL4} />
-
-      <div>
-        <button>
-          <DownloadJSON data= {[{L1: countL1}, {L2: countL2}, {L3: countL3}, {L4: countL4}]}/>
-        </button>
-      </div>
+        <button onClick={handleSubmit}/>
     </div>
   );
 }
