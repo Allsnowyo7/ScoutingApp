a0d7ebf05d81c6a6eab66bd2cd40ef765ed3ae46
Allsnowyo7 <edelvalle26@vcschools.org>
2025-10-22 00:34:29 -0700

    updating package.json

--- src/App.js
+++ src/App.js
@@ -11,23 +11,23 @@ const [countL2, setCountL2] = useState(0);
 const [countL3, setCountL3] = useState(0);
 const [countL4, setCountL4] = useState(0);
 
-const handleSubmit = (e) => {
-  e.preventDefault();
+// const handleSubmit = (e) => {
+//   e.preventDefault();
 
-  const objt = ["Match: "];
+//   const objt = ["Match: 1"];
 
-  axios
-    .post(
-      'https://script.google.com/macros/s/AKfycbwNluKKBqlj7pUOjcYYnyCfO86rPxd5E4FJixmpldCHjkY5u8LP2woVBfJNnamv1gA/exec',
-      ["Match: 1"]
-    )
-    .then((response) => {
-      console.log(response);
-    });
-};
+//   axios
+//     .post(
+//       'https://script.google.com/macros/s/AKfycbwNluKKBqlj7pUOjcYYnyCfO86rPxd5E4FJixmpldCHjkY5u8LP2woVBfJNnamv1gA/exec',
+//       ["Match: 1"]
+//     )
+//     .then((response) => {
+//       console.log(response);
+//     });
+// };
  return (
    <div>
-        <button onClick={handleSubmit}/>
+    <h1> test </h1>
     </div>
   );
 }
